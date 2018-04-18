ca4915e689d588cee317c123135de971b4c45daf
Robert Martinez <roshi@roberts-mbp.attlocal.net>
2018-04-17 19:31:44 -0700

    updated scrapper path

--- scrapper/streaming_twtr_scrap.py
+++ scrapper/streaming_twtr_scrap.py
@@ -1,15 +1,17 @@
-# -*- coding: utf-8 -*-
+# -* coding: utf-8 -*-
 from tweepy import Stream
 from tweepy.streaming import StreamListener
 from authentication import authenticate
 import json
 import argparse
+import os
 
 class Listener(StreamListener):
 
     def on_data(self, data):
         try:
-            with open('ai_data.json', 'a') as f:
+            data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data'))
+            with open(data_path + 'data.json', 'a') as f:
                 # data = json.dumps(data, ensure_ascii=False).encode("utf8")
                 # Check if lang = 'en'
                 tweet = json.loads(data)
@@ -31,12 +33,13 @@ def get_arguments():
     parser = argparse.ArgumentParser(description='Twitter Scraper')
     parser.add_argument("--filter_by", type=str, nargs="*",help="Labels or hashtags used to filter tweets")
     args = parser.parse_args()
-    print args.filter_by
+    #print args.filter_by
     return args.filter_by
 
 if __name__ == "__main__":
     get_arguments()
+    print("Authenticating...")
     twitter_stream = Stream(authenticate(), Listener())
-    print("calling twitter_stream.filter()")
+    print("Scrapping Twitter...")
     twitter_stream.filter(track=get_arguments())
 
