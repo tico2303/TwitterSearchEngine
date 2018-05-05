f7bd81c7e61243440ea535adb7015bee67617edc
Robert Martinez <roshi@roberts-mbp.attlocal.net>
2018-05-04 17:18:41 -0700

    added optional output directory to streaming_twter_scrap.py

--- scrapper/streaming_twtr_scrap.py
+++ scrapper/streaming_twtr_scrap.py
@@ -7,13 +7,21 @@ import argparse
 import os
 
 class Listener(StreamListener):
+    def __init__(self, outdir=None):
+        self.outdir = outdir
 
     def on_data(self, data):
         try:
-            data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data'))
-            data_path +="/"
-            with open(data_path + 'data.json', 'a') as f:
-                # data = json.dumps(data, ensure_ascii=False).encode("utf8")
+            if self.outdir == None:
+                data_path = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..', 'data'))
+            else:
+                data_path = self.outdir
+            if data_path[-1] != "/":
+                data_path +="/"
+
+            filename = data_path +'data.json'
+            print "opening file: ",filename
+            with open(filename, 'a') as f:
                 # Check if lang = 'en'
                 tweet = json.loads(data)
                 if tweet['lang'] == "en":
@@ -32,15 +40,18 @@ class Listener(StreamListener):
 
 def get_arguments():
     parser = argparse.ArgumentParser(description='Twitter Scraper')
-    parser.add_argument("--filter_by", type=str, nargs="*",help="Labels or hashtags used to filter tweets")
+    parser.add_argument("--outdir", type=str, nargs="*",help="Labels or hashtags used to filter tweets")
     args = parser.parse_args()
-    #print args.filter_by
-    return args.filter_by
+    # print "outdir: ", args.outdir
+    if args.outdir:
+        return args.outdir[0]
+    return None
 
 if __name__ == "__main__":
-    get_arguments()
+    outdir = get_arguments()
     print("Authenticating...")
-    twitter_stream = Stream(authenticate(), Listener())
+    twitter_stream = Stream(authenticate(), Listener(outdir))
     print("Scrapping Twitter...")
-    twitter_stream.filter(track=get_arguments())
+    # twitter_stream.filter(track=get_arguments())
+    twitter_stream.sample()
 
